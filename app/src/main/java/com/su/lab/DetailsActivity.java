d57d3162d765a16dbc346f0c8d538d6991fd5bc6
demina-anastasia <dyomina.nastya@gmail.com>
2020-10-28 12:01:01 +0400

    Сделали три TODO

--- app/src/main/java/com/su/lab/DetailsActivity.java
+++ app/src/main/java/com/su/lab/DetailsActivity.java
@@ -46,8 +46,11 @@ public class DetailsActivity extends Activity implements LoaderManager.LoaderCal
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
         cursor.moveToPosition(0);
         final String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + number));
+        startActivity(intent);
         /*
-         * TOD #3 Добавить обработчик клика и добавить логику перехода в системное приложение телефона
+         * TODO #3 Добавить обработчик клика и добавить логику перехода в системное приложение телефона
          */
 //        TextView phoneNumber = findViewById(R.id.contacts_phone_number);
 //        phoneNumber.setText(number);
